94f0f041935e89ff13c68a59c582eba42024a275
eddy <happytony1995@gmail.com>
2017-05-13 17:09:41 +0300

    Fix test

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Provider } from 'react-redux'
+
+import configureStore from '../../store'
+
+import App from './App'
+
+const store = configureStore()
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div)
+})
