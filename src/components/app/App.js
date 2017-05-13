30726315b232b0afdb81f7d3986d982123d09d14
eddy <happytony1995@gmail.com>
2017-05-13 16:15:08 +0300

    Add bootstrap

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,42 @@
 // @flow
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
 import Saga from '../saga/Saga'
 
-import logo from './logo.svg';
-import './App.css';
-
 class App extends Component {
-
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Saga />
+      <div>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to React</h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+          <Saga />
+          </Grid>
+        </Jumbotron>
       </div>
     );
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import configureStore from './store'
 
 import App from './components/app/App'
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap-theme.css'
+// import './bootstrap.min.css'
 import './index.css'
 
 const store = configureStore()
